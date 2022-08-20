48a6b9f6b057d08a4911ccb5fc1dc058f1e33a54
Titiwat Ruangtaweroj <titiwat.r@gmail.com>
2022-08-20 10:47:14 +0700

    add res.redirect() example + add comments

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 const userRouter = require('./routers/users')
 
+// set view engine to use ejs
 app.set('view engine', 'ejs')
 
+// set static folder
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 
+// see https://expressjs.com/en/api.html  for full detail on options
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({limit: '2mb'}))
+
+// declare middleware functions
 function middleware1(req, res, next) {
     console.log('-- middleware 1 --')
 
@@ -33,14 +38,17 @@ function specificMiddleware4(req, res, next) {
     next()
 }
 
+// global middleware
 app.use(middleware1)
 app.use(middleware2)
 
+// routers
 app.use('/users', userRouter)
 
+// end points
 app.get('/api', specificMiddleware3, specificMiddleware4, (req, res) => {
     // res.status(500).send('There is an error')
-    res.status(200).json({"message": 'welcome'})
+    res.status(200).json({message: 'welcome'})
     // res.download('/path/to/file')
 })
 
@@ -48,6 +56,13 @@ app.get('/render', (req, res) => {
     res.render('dummy', {mytext: 'A quick brown fox' })
 })
 
+app.get('/test_redirect', (req, res) => {
+    res.redirect('/redirected')
+})
 
+app.get('/redirected', (req, res) => {
+    res.send('you have been redirected to this page.')
+})
 
+// start server
 app.listen(3000)
